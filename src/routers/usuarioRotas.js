4448ae9cbd65a34f0abf0e2b855a4f86b1eaffa9
Diana Louise <111836528+DianaLouise02@users.noreply.github.com>
2024-10-03 16:40:50 -0300

    Add files via upload

---
+++ src/routers/usuarioRotas.js
@@ -0,0 +1,21 @@
+const { Router} = require("express");
+
+const router = Router();
+
+// Função de criar
+router.post('/' , );
+
+// Função de editar
+router.put('/:id', );
+
+// Função de deletar
+router.delete('/:id', );
+
+// Função de busca única
+router.get('/:id', );
+
+// Função de busca geral
+
+router.get('/', );
+
+module.exports = router;
---
+++ src/services/userService.js
@@ -0,0 +1,64 @@
+const User = require("../models/user")
+// app.js -> router.js -> userRoutes.js -> userController.js
+// -> userService.js -> model -> DATABASE
+const userService = {
+    create: async (user) => {
+        try {
+            return await User.create(user);
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao Criar User');
+        }
+    },
+
+    update: async (id, userToUpdate) => {
+        try {
+            const user = await User.findByPk(id);
+            if (!user) { // user for vazio
+                return null;
+            }
+            await user.update(userToUpdate);
+            await user.save();
+            return user;
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao Atualizar User');
+        }
+    },
+    getById: async (id) => {
+        try {
+
+            const user = await User.findByPk(id);
+            if (!user) {
+                return null;
+            }
+            return user;
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao buscar unico user');
+        }
+    },
+
+    getAll: async () => {
+        try {
+            return await User.findAll();
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao buscar todos os Users');
+        }
+    },
+
+    delete: async (id) => {
+        try {
+            const user = await User.findByPk(id);
+            if (!user) {
+                return null;
+            }
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao Deletar o User');
+        }
+    },
+    
+}
+
+
+
+
+
+module.exports = userService;
